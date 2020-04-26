17cfa8cdea3027df80095c184c83b55c0c539fa4
Christopher Shi <christopher.shi22@gmail.com>
2020-04-27 02:33:22 +1000

    added basic blog styling and react hooks

--- client/src/App/Blog/index.js
+++ client/src/App/Blog/index.js
@@ -1,53 +1,54 @@
-import React, {Component, Fragment} from 'react';
+import React, { useState, Fragment } from 'react';
 import axios from 'axios';
-import {BlogTile} from './BlogTile';
 import Divider from '@material-ui/core/Divider';
+// import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+// local imports 
+import {BlogTile} from './BlogTile';
 import './index.css';
 
-class Blog extends Component {
-  state = {
-      blogs: []
-  }
 
-	componentDidMount() {
-		this.getBlogs();
-	}
+// use react state hooks to convert class into react hook function?
 
-	getBlogs = () => {
-		axios.get('/blogs')
-			.then(res => {
-				if (res.data) {
-					this.setState({
-						blogs: res.data
+const useStyles = makeStyles((theme) => ({
+	markdown: {
+		...theme.typography.body2,
+		padding: theme.spacing(3, 0),
+	},
+}));
+
+function Blog() {
+	const classes = useStyles();
+	const [blogs, setBlogs] = useState([])
+
+	axios.get('/blogs')
+		.then(res => {
+			setBlogs(res.data);
+		})
+		.catch(err => console.log(err))
+
+
+	return(
+		<Fragment>
+			{
+				blogs && blogs.length > 0 ?
+				(
+					blogs.map(blog => {
+						return (
+							<Fragment>
+								<Divider />
+								<BlogTile className={classes.markdown} text={blog.markdown} />
+								
+							</Fragment>
+						)
 					})
-				}
-			})
-			.catch(err => console.log(err))
-	}
-
-	render() {
-		let { blogs } = this.state;
-		return(
-			<Fragment>
-				{
-					blogs && blogs.length > 0 ?
-					(
-						blogs.map(blog => {
-							return (
-								<Fragment>
-									<BlogTile text={blog.markdown} />
-									<Divider />
-								</Fragment>
-							)
-						})
-					) : (
-						<>No blogs</>
-					)
-				}
-			</Fragment>
-
-		)
-	}
-}
+				) : (
+					<>No blogs</>
+				)
+			}
+		</Fragment>
+	)
+};
 
 export default Blog;
